8a8a28276d455c5f54a66b258362c91ae5282957
jcamposv <jairocamposv@gmail.com>
2026-04-16 14:05:51 -0600

    updated image

--- .vercel/output/functions/_render.func/dist/server/entry.mjs
+++ .vercel/output/functions/_render.func/dist/server/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CmTqLKDD.mjs';
-import { manifest } from './manifest_pDHmmorA.mjs';
+import { manifest } from './manifest_BmhKxRNl.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -21,7 +21,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "f58712cb-bbda-49e7-b913-77be32a18cbf",
+    "middlewareSecret": "fb9f9e2f-021e-4216-aa66-0fcc21e7da8c",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
